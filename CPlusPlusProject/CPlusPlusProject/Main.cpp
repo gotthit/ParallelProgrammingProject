d5225d62aef20e3abb728480d51b2380572949c3
Andrey <andry@logvin.net>
2017-11-26 17:20:59 +0200

    Created projects

---
+++ CPlusPlusProject/CPlusPlusProject/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <stdio.h>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <bitset>
+#include <ctime>
+
+int main()
+{
+
+
+
+    system("pause");
+    return 0;
+}
